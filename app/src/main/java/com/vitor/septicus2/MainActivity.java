b150fa9814764d46ba265aeb04a5d449644c7b4e
VitorAugusto <vitoraugusto2508@gmail.com>
2019-08-01 14:21:35 -0300

    primeiro commit SEPTICUS 2 - Dimensionamento de tanques sépticos

---
+++ app/src/main/java/com/vitor/septicus2/MainActivity.java
@@ -0,0 +1,22 @@
+package com.vitor.septicus2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void dimensionarClick(View v){ //quando o usuário clicar em DIMENSIONAR
+
+        Intent intent = new Intent(this, activity_dados.class);
+        startActivity(intent);
+    }
+}
