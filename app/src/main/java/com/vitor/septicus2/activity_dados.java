03cd4d5e2f34475611958df36f13b8a9d57f9494
VitorAugusto <vitoraugusto2508@gmail.com>
2019-08-05 13:53:05 -0300

    mudanças

--- app/src/main/java/com/vitor/septicus2/activity_dados.java
+++ app/src/main/java/com/vitor/septicus2/activity_dados.java
@@ -1,8 +1,13 @@
 package com.vitor.septicus2;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class activity_dados extends AppCompatActivity {
 
@@ -10,5 +15,87 @@ public class activity_dados extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dados);
+    }
+
+
+    public boolean todosOsCamposPreenchidos(){
+
+        EditText campoNumeroPessoas = (EditText) findViewById(R.id.campoNumeroPessoas);
+
+        RadioGroup padraoResidenciaRadioGroup = (RadioGroup) findViewById(R.id.padraoResidenciaRadioGroup);
+
+        RadioGroup tipoResidenciaRadioGroup = (RadioGroup) findViewById(R.id.tipoResidenciaRadioGroup);
+
+        RadioGroup temperaturaMediaRadioGroup = (RadioGroup) findViewById(R.id.radioGroupTemperaturaMedia);
+
+        RadioGroup intervaloLimpezaRadioGroup = (RadioGroup) findViewById(R.id.radioIntervaloLimpeza);
+
+        RadioGroup geometriaTanqueRadioGroup = (RadioGroup) findViewById(R.id.radioGeometriaTanque);
+
+        EditText campoProfundidade = (EditText) findViewById(R.id.campoProfundidade);
+
+        EditText campoInfiltracao = (EditText) findViewById(R.id.campoInfiltracao);
+
+        if(campoTextoVazio(campoNumeroPessoas)
+            ||
+                campoTextoVazio(campoProfundidade)
+            ||
+                campoTextoVazio(campoInfiltracao)
+            ||
+                botaoRadioVazio(padraoResidenciaRadioGroup)
+            ||
+                botaoRadioVazio(tipoResidenciaRadioGroup)
+            ||
+                botaoRadioVazio(temperaturaMediaRadioGroup)
+            ||
+                botaoRadioVazio(intervaloLimpezaRadioGroup)
+            ||
+                botaoRadioVazio(geometriaTanqueRadioGroup)){
+
+            return false;
+        }else{
+            return true;
+        }
+
+
+    }
+
+    public String getBotaoSelecionado(RadioGroup g){
+
+        return null;
+    }
+
+    public boolean campoTextoVazio(EditText myText){
+
+        return myText.getText().toString().trim().length() == 0;
+    }
+
+    public boolean botaoRadioVazio(RadioGroup myRadio){
+
+        if(myRadio.getCheckedRadioButtonId() == -1 ){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+
+    public void mensagemErro(){
+        //IMPLEMENTAR MENSAGEM DE ERRO FULL
+
+        System.out.println("CAMPOS NÃO PREENCHIDOS TOTALMENTE !!!");
+    }
+
+    public void mensagemSucesso(){
+        System.out.println("SUCESSO !! ");
+    }
+
+    public void gerarMemorial(View v){
+
+        if(todosOsCamposPreenchidos()){
+            mensagemSucesso();
+        }else{
+            mensagemErro();
+        }
     }
 }
